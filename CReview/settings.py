7c53e934f7517621f7a07c78eb454f9880ee80bb
esp-ram <esperon.ramiro@gmail.com>
2021-07-02 21:41:14 -0300

    Change default database to postgreSQL

--- CReview/settings.py
+++ CReview/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 from dotenv import load_dotenv, find_dotenv
 import os
+import psycopg2
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -80,8 +81,11 @@ WSGI_APPLICATION = 'CReview.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("NAME"),
+        'USER': os.environ.get("DBUSER"),
+        'PASSWORD': os.environ.get("PASSWORD"),
+        'HOST': os.environ.get("HOST"),
     }
 }
 
